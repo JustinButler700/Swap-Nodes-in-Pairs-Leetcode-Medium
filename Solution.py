886f0496f95ccd5e739e86f60faebedb0b4b4ed1
Justin Butler <57680670+JustinButler700@users.noreply.github.com>
2023-01-24 09:38:16 -0800

    Create Solution.py

---
+++ Solution.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        dummy = ListNode(0, head)
+        prev = dummy
+        curr = head
+        while curr and curr.next :
+            #save ptr vals
+            nextNode = curr.next.next
+            second = curr.next
+
+            #update and swap values
+            second.next = curr
+            curr.next = nextNode
+            prev.next = second
+
+            #iterate
+            prev = curr
+            curr = nextNode
+        return dummy.next
